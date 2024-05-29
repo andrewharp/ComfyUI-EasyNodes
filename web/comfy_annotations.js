13648511d26f55d3daaef7dd7d8234ea920c6311
andrewharp <andrew.harp@gmail.com>
2024-05-29 13:12:15 -0700

    Remove dead code

--- web/comfy_annotations.js
+++ web/comfy_annotations.js
@@ -11,7 +11,7 @@ app.registerExtension({
       type: "text",
       defaultValue: "",
     });
-    
+
     app.ui.settings.addSetting({
       id: "Comfy.EditorPathPrefix",
       name: "Stack trace link prefix (insert this in stack traces to make them clickable, e.g. 'vscode://vscode-remote/wsl+Ubuntu')",
@@ -30,7 +30,7 @@ app.registerExtension({
       var bgColor = null;
       var nodeSource = null;
       var outputLines = [];
-      
+
       for (const line of lines) {
         if (line.startsWith('ComfyUINodeColor=')) {
           const color = line.split('=')[1];
@@ -45,10 +45,10 @@ app.registerExtension({
           outputLines.push(line);
         }
       }
-      
+
       nodeData.description = outputLines.join('\n');
       const editorPathPrefix = app.ui.settings.getSettingValue("Comfy.EditorPathPrefix");
-      
+
       function applyColorsAndSource() {
         if (fgColor) {
           this.color = fgColor;
@@ -61,14 +61,14 @@ app.registerExtension({
         }
         this.description = nodeData.description;
       }
-      
+
       // Apply colors and source location when the node is created
       const onNodeCreated = nodeType.prototype.onNodeCreated;
       nodeType.prototype.onNodeCreated = function () {
         onNodeCreated?.apply(this, arguments);
         applyColorsAndSource.call(this);
       };
-      
+
       // Apply colors and source location when configuring the node
       const onConfigure = nodeType.prototype.onConfigure;
       nodeType.prototype.onConfigure = function () {
@@ -80,151 +80,61 @@ app.registerExtension({
 });
 
 
-// function getOutputNodesFromSelected(canvas) {
-//     return ((canvas.selected_nodes &&
-//         Object.values(canvas.selected_nodes).filter((n) => {
-//             var _a;
-//             return (n.mode != LiteGraph.NEVER &&
-//                 ((_a = n.constructor.nodeData) === null || _a === void 0 ? void 0 : _a.output_node));
-//         })) ||
-//         []);
-// }
-
-// function showQueueNodesMenuIfOutputNodesAreSelected(existingOptions, node) {
-//     if (CONFIG_SERVICE.getConfigValue("features.menu_queue_selected_nodes") != false) {
-//         const canvas = app.canvas;
-//         const outputNodes = getOutputNodesFromSelected(canvas);
-//         const menuItem = {
-//             content: `Queue Selected Output Nodes (rgthree) &nbsp;`,
-//             className: "rgthree-contextmenu-item",
-//             callback: () => {
-//                 rgthree.queueOutputNodes(outputNodes.map((n) => n.id));
-//             },
-//             disabled: !outputNodes.length,
-//         };
-//         let idx = existingOptions.findIndex((o) => (o === null || o === void 0 ? void 0 : o.content) === "Outputs") + 1;
-//         idx = idx || existingOptions.findIndex((o) => (o === null || o === void 0 ? void 0 : o.content) === "Align") + 1;
-//         idx = idx || 3;
-//         existingOptions.splice(idx, 0, menuItem);
-//     }
-//     return existingOptions;
-// }
-
-// app.registerExtension({
-//     name: "open_file_in_editor",
-//     async beforeRegisterNodeDef(nodeType, nodeData) {
-//         const getExtraMenuOptions = nodeType.prototype.getExtraMenuOptions;
-//         nodeType.prototype.getExtraMenuOptions = function (canvas, options) {
-//             getExtraMenuOptions ? getExtraMenuOptions.apply(this, arguments) : undefined;
-//             showQueueNodesMenuIfOutputNodesAreSelected(options, this);
-//         };
-//     },
-//     async setup() {
-//         console.log("rgthree-menu-setup");
-//         const getCanvasMenuOptions = LGraphCanvas.prototype.getCanvasMenuOptions;
-//         LGraphCanvas.prototype.getCanvasMenuOptions = function (...args) {
-//             const options = getCanvasMenuOptions.apply(this, [...args]);
-//             showQueueNodesMenuIfOutputNodesAreSelected(options);
-//             return options;
-//         };
-//     },
-// });
-
-
-
-// app.registerExtension({
-//     name: "ComfyUI-Annotations",
-//     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-//         const onNodeCreated = nodeType.prototype.onNodeCreated;
-//         nodeType.prototype.onNodeCreated = function () {
-//             onNodeCreated?.apply(this, arguments);
-//             this.preview_images = [];
-//         };
-
-//         const onDrawBackground = nodeType.prototype.onDrawBackground;
-//         nodeType.prototype.onDrawBackground = function (ctx) {
-//             onDrawBackground?.apply(this, arguments);
-//             if (this.preview_images && this.preview_images.length > 0) {
-//                 const imgElement = ctx.querySelector("img");
-//                 if (imgElement) {
-//                     const imageData = this.preview_images[0];
-//                     imgElement.src = `view?filename=${imageData.filename}&type=${imageData.type}&subfolder=${imageData.subfolder}`;
-//                 }
-//             }
-//         };
-
-//         const onExecuted = nodeType.prototype.onExecuted;
-//         nodeType.prototype.onExecuted = function (message) {
-//             onExecuted?.apply(this, arguments);
-//             console.log(message);
-//             if (message.images) {
-//                 this.preview_images = message.images;
-//                 console.log(message.images);
-//                 this.setDirtyCanvas(true);
-//             }
-//         };
-//     },
-// });
-
-
 app.registerExtension({
-	name: "ComfyUI-Annotations-ImagePreview",
+  name: "ComfyUI-Annotations-ImagePreview",
 
-	nodeCreated(node, app) {
-		if(node.comfyClass == "AnythingCache") {
-			node._imgs = [new Image()];
-			node.imageIndex = 0;
+  nodeCreated(node, app) {
+    if (node.comfyClass == "AnythingCache") {
+      node._imgs = [new Image()];
+      node.imageIndex = 0;
 
       let set_img_act = (v) => {
-				node._img = v;
-				var canvas = document.createElement('canvas');
-				canvas.width = v[0].width;
-				canvas.height = v[0].height;
+        node._img = v;
+        var canvas = document.createElement('canvas');
+        canvas.width = v[0].width;
+        canvas.height = v[0].height;
 
-				var context = canvas.getContext('2d');
-				context.drawImage(v[0], 0, 0, v[0].width, v[0].height);
-
-				// var base64Image = canvas.toDataURL('image/png');
-				// w.value = base64Image;
-			};
+        var context = canvas.getContext('2d');
+        context.drawImage(v[0], 0, 0, v[0].width, v[0].height);
+      };
 
       let already_tried = false;
 
-			Object.defineProperty(node, 'imgs', {
-				set(v) {
+      Object.defineProperty(node, 'imgs', {
+        set(v) {
           console.log("Setting images");
           console.log(v);
-					if(v && v.length == 0){
+          if (v && v.length == 0) {
             console.log("No images!");
-          	return;
+            return;
           }
-					node._imgs = v;
-				},
-				get() {
+          node._imgs = v;
+        },
+        get() {
           if (!already_tried) {
             already_tried = true;
             try {
-              if(node._imgs[0].src == '') {
+              if (node._imgs[0].src == '') {
                 const subfolder = "ComfyUI-Annotations";
                 const type = "temp";
                 const filename = `preview-${node.id}.png`;
 
                 let params = `?filename=${filename}&type=${type}&subfolder=${subfolder}`;
 
-                api.fetchApi('/view/validate'+params, { cache: "no-store" }).then(response => {
-                  if(response.status == 200) {
-                    console.log("Got image!");      
-                    node._imgs[0].src = 'view'+params;
+                api.fetchApi('/view/validate' + params, { cache: "no-store" }).then(response => {
+                  if (response.status == 200) {
+                    console.log("Got image!");
+                    node._imgs[0].src = 'view' + params;
                   }
-                });      
+                });
               }
             } catch (e) {
             }
           }
 
           return node._imgs;
-				}
-			});
-		}
-	}
+        }
+      });
+    }
+  }
 })
