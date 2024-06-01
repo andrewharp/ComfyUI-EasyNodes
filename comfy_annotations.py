ffea87fa3249f277d5fb5e6348571df2bd6d25df
andrewharp <andrew.harp@gmail.com>
2024-05-31 17:11:17 -0700

    Explicitly list the public easy_nodes interface rather than using a global import.

--- comfy_annotations.py
+++ comfy_annotations.py
@@ -1,16 +1,5 @@
 # This file is deprecated, and provided only for backwards compatibility with the original comfy_annotations module.
 import logging
+from easy_nodes import * # noqa: F403
 
-from easy_nodes.easy_nodes import (  # noqa: F401
-    Choice,
-    ComfyNode,
-    ImageTensor,
-    NumberInput,
-    StringInput,
-    create_field_setter_node,
-    register_type,
-)
-
-logging.warning("comfy_annotations is deprecated. Please use easy_nodes instead.")
-
-ComfyFunc = ComfyNode
+logging.warning("comfy_annotations is deprecated. Please import from easy_nodes instead.")
--- easy_nodes/__init__.py
+++ easy_nodes/__init__.py
@@ -1,1 +1,17 @@
-from .easy_nodes import *  # noqa: F401 F403
+from easy_nodes.easy_nodes import (  # noqa: F401
+    AnyType,
+    Choice,
+    ComfyNode,
+    ImageTensor,
+    MaskTensor,
+    NumberInput,
+    StringInput,
+    show_image,
+    show_text,
+    create_field_setter_node,
+    initialize_easy_nodes,
+    register_type,
+)
+
+# For backwards compatibility with the original comfy_annotations module.
+ComfyFunc = ComfyNode
