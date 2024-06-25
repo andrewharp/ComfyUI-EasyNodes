a9004f3617991f1c070b7f77fc5eab4ad70ad6ec
Ocyss_04 <yuntu@ocyss.icu>
2024-06-25 18:04:38 +0800

    Pull over export fix

--- easy_nodes/__init__.py
+++ easy_nodes/__init__.py
@@ -11,6 +11,7 @@ from easy_nodes.easy_nodes import (  # noqa: F401
     create_field_setter_node,
     initialize_easy_nodes,
     register_type,
+    get_node_mappings
 )
 
 # For backwards compatibility with the original comfy_annotations module.
